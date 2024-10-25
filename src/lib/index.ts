b0f14a79eb7a48f2f34a8eeb26d852fd6ced6b22
Nate Diven <natediven1@gmail.com>
2024-10-25 14:25:25 -0600

    added exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,9 @@ export { default as MapLibre } from './MapLibre.svelte';
 export { default as NavigationControl } from './NavigationControl.svelte';
 export { default as GeoJSON } from './GeoJSON.svelte';
 export { default as Layer } from './Layer.svelte';
+export { default as FillLayer } from './FillLayer.svelte';
+export { default as Marker } from './Marker.svelte';
+export { default as DefaultMarker } from './DefaultMarker.svelte';
 
 export { getId, mapContext, sourceContext } from './context.svelte';
 export * from './types.js';

3a49073c7adc09f88aa2480e205c411d6871100d
Nate <natediven1@gmail.com>
2024-10-25 00:16:45 -0600

    added FillLayer component

---
+++ src/lib/FillLayer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Layer from './Layer.svelte';
+  import type { ComponentProps } from 'svelte';
+
+  type Props = Omit<ComponentProps<typeof Layer>, 'type'>;
+
+  let props: Props = $props();
+</script>
+
+<Layer {...props} type="fill" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import FillLayer from '$lib/FillLayer.svelte';
+  import { isTextLayer } from '$lib/filters.js';
   import GeoJson from '$lib/GeoJSON.svelte';
   import Layer from '$lib/Layer.svelte';
   import MapLibre from '$lib/MapLibre.svelte';
@@ -8,13 +10,30 @@
   let show = $state(true);
   let map = $state<maplibregl.Map | null>(null);
 
-  let colors = ['#ff000055', '#00ff0055', '#0000ff55'];
-  let currentColor = 0;
-  let paint = $state({ 'fill-color': colors[0] });
+  let style = $state<string>('https://tiles.openfreemap.org/styles/bright');
+  // let style = $state<string>('https://basemaps.cartocdn.com/gl/positron-gl-style/style.json');
+
+  let colors = [
+    '#ff000055',
+    '#00ff0055',
+    '#0000ff55',
+    '#ff00ff55',
+    '#00ffff55',
+    '#ffff0055',
+    '#ffffff55',
+    '#00000055',
+  ];
+  let currentColor = $state(0);
+
+  function getPaint() {
+    return { 'fill-color': colors[currentColor] };
+  }
+
+  let paint = $state(getPaint());
 
   function changeColor() {
     currentColor = (currentColor + 1) % colors.length;
-    paint = { 'fill-color': colors[currentColor]! };
+    paint = getPaint();
   }
 </script>
 
@@ -30,20 +49,26 @@
 
 <div class="grid min-h-screen w-full place-items-center bg-orange-100">
   <div class="mx-auto flex w-full max-w-5xl flex-col gap-6 px-4 py-8">
-    <MapLibre
-      class="aspect-video w-full overflow-clip rounded-lg border-2 drop-shadow-lg"
-      style="https://tiles.openfreemap.org/styles/bright"
-      center={[-73.9598939, 40.7896239]}
-      zoom={10}
-      bind:map
-    >
-      {#if show}
-        <NavigationControl />
-        <GeoJson data={manhattan as any}>
-          <Layer type="fill" {paint} />
-        </GeoJson>
-      {/if}
-    </MapLibre>
+    <div class="flex flex-col gap-2">
+      <MapLibre
+        class="aspect-video w-full overflow-clip rounded-lg border-2 drop-shadow-lg"
+        {style}
+        center={[-73.9598939, 40.7896239]}
+        zoom={10.3}
+        bind:map
+        filterLayers={(l) => !isTextLayer(l, 'openmaptiles')}
+      >
+        {#if show}
+          <NavigationControl />
+          <GeoJson data={manhattan as any}>
+            <FillLayer {paint} onclick={console.log} />
+          </GeoJson>
+        {/if}
+      </MapLibre>
+      <div>
+        <span class="font-mono">{colors[currentColor]}</span>
+      </div>
+    </div>
     <div class="flex justify-evenly">
       <div class="flex flex-1 items-center gap-2">
         <kbd>T</kbd>
